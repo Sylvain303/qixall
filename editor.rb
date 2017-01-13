276136e8cc6a01c9ac3064b47f3bd8453902001c
Sylvain Viart <sylvain303@github>
2017-01-13 08:46:53 +0100

    add editor removing white space qixall

--- editor.rb
+++ editor.rb
@@ -1,23 +1,22 @@
-#vim: set fdm=marker ts=2 sw=2 shellslash commentstring=#%s:
+# vim: set fdm=marker ts=2 sw=2 shellslash commentstring=#%s:
 # coding: utf-8
+#
+# Game editor only
+#
+
 require 'rubygems'
 require 'bundler/setup'
 $:.push('.')
 require 'gosu'
 require 'coord'
-require 'player'
 require 'polygon'
 require 'area'
-require 'star'
 
 require 'pry'
 require 'pry-nav'
 
-#require 'debugger'
-#Debugger.start(:post_mortem => true)
-
 module ZOrder
-  Background, Grid, Lines, Stars, Monster, Player, UI, Mouse = *0..20
+  Background, Monster, Grid, Lines, UI, Mouse = *0..20
 end
 
 # Config# {{{
@@ -59,6 +58,7 @@ class Monster#{{{
 	end
 
 	attr_reader :me
+  attr_accessor :factor
 
 	def start(area)
 		@area = area
@@ -71,6 +71,8 @@ class Monster#{{{
 	def draw
 		@image.draw_rot(@me.x, @me.y, ZOrder::Monster, @angle, 0.5, 0.5, @factor, @factor)
 		@window.draw_point(@me)
+    # draw bounding box
+    @b.draw
 	end
 
 	def move
@@ -116,25 +118,6 @@ class Monster#{{{
 	end
 end#}}}
 
-class Tropedo#{{{
-	def initialize(window, dir)
-		@window = window
-		@dir = dir
-		@image = Gosu::Image.new(@window, "media/torpedo.png", false)
-		@angle = 90.0
-	end
-
-	def move
-		@x += @dir.x
-		@y += @dir.y
-	end
-
-	def draw
-		@image.draw_rot(@dir.x, @dir.x, ZOrder::Monster, @angle)
-	end
-
-end#}}}
-
 class Playground#{{{
 	def initialize(window)
 		@window = window
@@ -178,9 +161,6 @@ class GameWindow < Gosu::Window#{{{
 		@monster = Monster.new(self)
 		@monster.start(@playground.area)
 
-		@player = Player.new(self)
-		@player.start(@playground.area, 0, 3)
-
 		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
 		@cursor = Gosu::Image.new(self, "media/Cursor.png", false)
 
@@ -188,42 +168,12 @@ class GameWindow < Gosu::Window#{{{
 		@area_last_loaded = {}
 		@show_grid = true
 
-		# stars from tutorial…
-		@star_anim = Gosu::Image::load_tiles(self, "media/Star.png", 25, 25, false)
-		@stars = Array.new
 	end#}}}
+
 	attr_reader :epais, :grid, :screen_h, :screen_w, :playground, :monster
 	attr_accessor :tail
 
 	def update#{{{
-
-
-		if button_down? Gosu::Button::KbLeft
-			@player.change_dir(:left)
-		end
-		if button_down? Gosu::Button::KbRight
-			@player.change_dir(:right)
-		end
-		if button_down? Gosu::Button::KbUp
-			@player.change_dir(:up)
-		end
-		if button_down? Gosu::Button::KbDown
-			@player.change_dir(:down)
-		end
-
-		if button_down? Gosu::Button::KbSpace
-			@player.stop
-		end
-
-		@player.move
-		@monster.move
-
-		# and some random stars to animated the game and test framed sprite code
-		if rand(100) < 4 and @stars.size < 25 then
-			@stars.push(Star.new(@star_anim))
-		elsif rand(100) < 10 and @stars.size > 10
-			@stars.shift()
-		end
 	end#}}}
 
 	def draw_point(p)#{{{
@@ -238,17 +188,14 @@ class GameWindow < Gosu::Window#{{{
 
 	def draw#{{{
 		@monster.draw
-		@player.draw
 		@playground.draw
 		@cursor.draw(mouse_x, mouse_y, ZOrder::Mouse)
-		@stars.each { |star| star.draw }
 
 		# info
 		@font.draw("mouse pos: #{mouse_x}, #{mouse_y}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
 		@font.draw("grid: #{@grid}", 470, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
 		@font.draw("clic: #{@click}", 10, 10 + 1*15, ZOrder::UI, 1.0, 1.0, 0xffffff00)
-		player_info = "player: #{@player.current_dir}, #{@player.next_dir}, #{@player.zone}"
-		@font.draw(player_info, 330, 10 + 1*15, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+
 		@font.draw("area: #{@playground.area.size}", 230, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
 
 		@area_click.draw if @area_click
@@ -273,7 +220,6 @@ class GameWindow < Gosu::Window#{{{
 		when Gosu::Button::KbF1
 			read_area(@playground.area, "data/playground*.txt")
 			i = @playground.area.size + rand(@playground.area.size)
-			@player.start(@playground.area, i % @playground.area.size, (i + 1) % @playground.area.size)
 		when Gosu::Button::KbF2
 			@show_grid = ! @show_grid
 		when Gosu::Button::KbF3
@@ -293,8 +239,6 @@ class GameWindow < Gosu::Window#{{{
 			puts "#{@click} #{@playground.area.inside?(@click.x, @click.y)}"
 		when Gosu::Button::MsRight
 			@area_click.empty!
-		when Gosu::Button::KbLeftControl, Gosu::Button::KbRightControl
-			@player.action(:down)
 		else
 			# some keybord letter
 			case button_id_to_char(id)
@@ -319,18 +263,16 @@ class GameWindow < Gosu::Window#{{{
 				else
 					puts "outside"
 				end
+			when '-'
+        @monster.factor += 0.1
+			when '+'
+        @monster.factor -= 0.1
 			end
 		end
 	end#}}}
 
-	def button_up(id)
-		case id
-		when Gosu::Button::KbLeftControl, Gosu::Button::KbRightControl
-			@player.action(:up)
-		end
-	end
-
 private
+  # load saved area
 	def read_area(area_ref, pattern)
 		found = false
 		first = nil
